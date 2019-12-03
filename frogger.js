b79565455fc796d55e55cc696f8b32dda344168f
Spencer Rhoton <rhotonspencer@gmail.com>
2019-12-03 15:57:22 -0700

    started drawing frog. need to find solution for square of frog

--- frogger.js
+++ frogger.js
@@ -1,32 +1,54 @@
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
 
-// grass
-ctx.fillStyle = "lime"
-ctx.fillRect(0, 440, 570,45)
-ctx.fillRect(0, 220, 570, 45)
+function drawBackground() {
+    // grass
+    ctx.fillStyle = "lime"
+    ctx.fillRect(0, 440, 570,45)
+    ctx.fillRect(0, 220, 570, 45)
+    
+    // road
+    ctx.beginPath()
+    ctx.strokeStyle = "white"
+    ctx.moveTo(0, 395)
+    ctx.lineTo(570, 395)
+    ctx.setLineDash([5])
+    ctx.strokeWidth = 2
+    ctx.stroke()
+    
+    ctx.beginPath()
+    ctx.strokeStyle = "white"
+    ctx.moveTo(0, 350)
+    ctx.lineTo(570, 350)
+    ctx.setLineDash([5])
+    ctx.strokeWidth = 2
+    ctx.stroke()
+    
+    ctx.beginPath()
+    ctx.strokeStyle = "white"
+    ctx.moveTo(0, 305)
+    ctx.lineTo(570, 305)
+    ctx.setLineDash([5])
+    ctx.strokeWidth = 2
+    ctx.stroke()
+    
+    // water
+    ctx.fillStyle = "blue"
+    ctx.fillRect(0, 0, 570, 220)
+}
 
-// road
-ctx.beginPath()
-ctx.strokeStyle = "white"
-ctx.moveTo(0, 395)
-ctx.lineTo(570, 395)
-ctx.setLineDash([5])
-ctx.strokeWidth = 2
-ctx.stroke()
+function drawFrog() {
+    ctx.beginPath()
+    ctx.rect(50, 444, 30, 30)
+    ctx.strokeStyle = "purple"
+    ctx.stroke()
+}
 
-ctx.beginPath()
-ctx.strokeStyle = "white"
-ctx.moveTo(0, 350)
-ctx.lineTo(570, 350)
-ctx.setLineDash([5])
-ctx.strokeWidth = 2
-ctx.stroke()
+function draw() {
+    drawBackground()
+    drawFrog()
 
-ctx.beginPath()
-ctx.strokeStyle = "white"
-ctx.moveTo(0, 305)
-ctx.lineTo(570, 305)
-ctx.setLineDash([5])
-ctx.strokeWidth = 2
-ctx.stroke()
+    requestAnimationFrame(draw)
+}
+
+draw()
