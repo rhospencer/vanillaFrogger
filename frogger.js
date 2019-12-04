ee45dd582f491d6fa38cbd17f4eeb6c3b8573cfc
Spencer Rhoton <rhotonspencer@gmail.com>
2019-12-04 14:43:03 -0700

    made image appear, working on frog movement functions

--- frogger.js
+++ frogger.js
@@ -1,9 +1,60 @@
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
 
+
+// frog variables
+let frog = new Image(); frog.src = 'frogger.png'
+let sx = 0
+let sy = 0
+let swidth = 40
+let sheight = 40
+let x = 50
+let y = 444
+let width = 30
+let height = 30
+
+// frog movement
+let rightPressed = false
+let leftPressed = false
+let upPressed = false
+let downPressed = false
+
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = true
+    }
+    if (e.keyCode == 37) {
+        leftPressed = true
+    }
+    if (e.keyCode == 38) {
+        upPressed = true
+    }
+    if (e.keyCode == 40) {
+        downPressed = true
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = false
+    }
+    if (e.keyCode == 37) {
+        leftPressed = false
+    }
+    if (e.keyCode == 38) {
+        upPressed = false
+    }
+    if (e.keyCode == 40) {
+        downPressed = false
+    }
+}
+
 function drawBackground() {
     // grass
-    ctx.fillStyle = "lime"
+    ctx.fillStyle = "purple"
     ctx.fillRect(0, 440, 570,45)
     ctx.fillRect(0, 220, 570, 45)
     
@@ -38,10 +89,7 @@ function drawBackground() {
 }
 
 function drawFrog() {
-    ctx.beginPath()
-    ctx.rect(50, 444, 30, 30)
-    ctx.strokeStyle = "purple"
-    ctx.stroke()
+    ctx.drawImage(frog, sx, sy, swidth, sheight, x, y, width, height)
 }
 
 function draw() {
