255e85aa7a33cfe62a596cb8e1b935d00fc67184
Spencer Rhoton <rhotonspencer@gmail.com>
2019-12-04 17:24:25 -0700

    frog will reset when hit by a car

--- frogger.js
+++ frogger.js
@@ -25,19 +25,19 @@ let left = true
 
 // car variables
 let car1_1 = new Image(); car1_1.src = 'car1.png'
-let car1X1 = 100
+let car1X1 = 100; let car1Y1 = 400; let car1Width = 47; let car1Height = 30;
 let car1_2 = new Image(); car1_2.src = 'car1.png'
 let car1X2 = 500
 let car2_1 = new Image(); car2_1.src = 'car2.png'
-let car2X1 = 150
+let car2X1 = 150; let car2Y1 = 355; let car2Width = 30; let car2Height = 30;
 let car2_2 = new Image(); car2_2.src = 'car2.png'
 let car2X2 = 400
 let car2_3 = new Image(); car2_3.src = 'car2.png'
 let car2X3 = 650
 let car3_1 = new Image(); car3_1.src = 'car3.png'
-let car3X1 = 500
+let car3X1 = 500; let car3Y1 = 310; let car3Width = 30; let car3Height = 30;
 let truck1_1 = new Image(); truck1_1.src = 'truck1.png'
-let truck1X1 = 450
+let truck1X1 = 450; let truck1Y1 = 265; let truck1Width = 108; let truck1Height = 30;
 let truck1_2 = new Image(); truck1_2.src = 'truck1.png'
 let truck1X2 = 310
 let truck1_3 = new Image(); truck1_3.src = 'truck1.png'
@@ -159,13 +159,13 @@ function moveFrog() {
 }
 
 function drawLane1() {
-    ctx.drawImage(car1_1, 0, 0, 47, 35, car1X1, 400, 47, 35)
+    ctx.drawImage(car1_1, 0, 0, 47, 35, car1X1, car1Y1, car1Width, car1Height)
     if (car1X1 < canvas.width + 100) {
         car1X1 = car1X1 + 4
     } else {
         car1X1 = -100
     }
-    ctx.drawImage(car1_2, 0, 0, 47, 35, car1X2, 400, 47, 35)
+    ctx.drawImage(car1_2, 0, 0, 47, 35, car1X2, car1Y1, car1Width, car1Height)
     if (car1X2 < canvas.width + 100) {
         car1X2 = car1X2 + 4
     } else {
@@ -174,19 +174,19 @@ function drawLane1() {
 }
 
 function drawLane2() {
-    ctx.drawImage(car2_1, 0, 0, 35, 35, car2X1, 355, 35, 35)
+    ctx.drawImage(car2_1, 0, 0, 35, 35, car2X1, car2Y1, car2Width, car2Height)
     if (car2X1 < canvas.width + 100) {
         car2X1 = car2X1 + 2
     } else {
         car2X1 = -100
     }
-    ctx.drawImage(car2_2, 0, 0, 35, 35, car2X2, 355, 35, 35)
+    ctx.drawImage(car2_2, 0, 0, 35, 35, car2X2, car2Y1, car2Width, car2Height)
     if (car2X2 < canvas.width + 100) {
         car2X2 = car2X2 + 2
     } else {
         car2X2 = -100
     }
-    ctx.drawImage(car2_3, 0, 0, 35, 35, car2X3, 355, 35, 35)
+    ctx.drawImage(car2_3, 0, 0, 35, 35, car2X3, car2Y1, car2Width, car2Height)
     if (car2X3 < canvas.width + 100) {
         car2X3 = car2X3 + 2
     } else {
@@ -195,7 +195,7 @@ function drawLane2() {
 }
 
 function drawLane3() {
-    ctx.drawImage(car3_1, 0, 0, 35, 35, car3X1, 310, 35, 35)
+    ctx.drawImage(car3_1, 0, 0, 35, 35, car3X1, car3Y1, car3Width, car3Height)
     if (car3X1 > canvas.width - 670) {
         car3X1 = car3X1 - 8
     } else {
@@ -204,23 +204,53 @@ function drawLane3() {
 }
 
 function drawLane4() {
-    ctx.drawImage(truck1_1, 0, 0, 108, 35, truck1X1, 265, 108, 35)
+    ctx.drawImage(truck1_1, 0, 0, 108, 35, truck1X1, truck1Y1, truck1Width, truck1Height)
     if (truck1X1 > canvas.width - 670) {
         truck1X1 = truck1X1 - 1
     } else {
         truck1X1 = 670
     }
-    ctx.drawImage(truck1_2, 0, 0, 108, 35, truck1X2, 265, 108, 35)
+    ctx.drawImage(truck1_2, 0, 0, 108, 35, truck1X2, truck1Y1, truck1Width, truck1Height)
     if (truck1X2 > canvas.width - 670) {
         truck1X2 = truck1X2 - 1
     } else {
         truck1X2 = 670
     }
-    ctx.drawImage(truck1_3, 0, 0, 108, 35, truck1X3, 265, 108, 35)
+    ctx.drawImage(truck1_3, 0, 0, 108, 35, truck1X3, truck1Y1, truck1Width, truck1Height)
     if (truck1X3 > canvas.width - 670) {
         truck1X3 = truck1X3 - 1
     } else {
         truck1X3 = 670
+    }
+}
+
+function runOver() {
+    if (car1X1 <= x + width && car1X1 + car1Width >= x && car1Y1 + car1Height >= y && car1Y1<= y + height) {
+        y = 488
+    }
+    if (car1X2 <= x + width && car1X2 + car1Width >= x && car1Y1 + car1Height >= y && car1Y1<= y + height) {
+        y = 488
+    }
+    if (car2X1 <= x + width && car2X1 + car2Width >= x && car2Y1 + car2Height >= y && car2Y1<= y + height) {
+        y = 488
+    }
+    if (car2X2 <= x + width && car2X2 + car2Width >= x && car2Y1 + car2Height >= y && car2Y1<= y + height) {
+        y = 488
+    }
+    if (car2X3 <= x + width && car2X3 + car2Width >= x && car2Y1 + car2Height >= y && car2Y1<= y + height) {
+        y = 488
+    }
+    if (car3X1 <= x + width && car3X1 + car3Width >= x && car3Y1 + car3Height >= y && car3Y1<= y + height) {
+        y = 488
+    }
+    if (truck1X1 <= x + width && truck1X1 + truck1Width >= x && truck1Y1 + truck1Height >= y && truck1Y1<= y + height) {
+        y = 488
+    }
+    if (truck1X2 <= x + width && truck1X2 + truck1Width >= x && truck1Y1 + truck1Height >= y && truck1Y1<= y + height) {
+        y = 488
+    }
+    if (truck1X3 <= x + width && truck1X3 + truck1Width >= x && truck1Y1 + truck1Height >= y && truck1Y1<= y + height) {
+        y = 488
     }
 }
 
@@ -233,6 +263,7 @@ function draw() {
     drawLane2()
     drawLane3()
     drawLane4()
+    runOver()
 
     requestAnimationFrame(draw)
 }
